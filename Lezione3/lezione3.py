405b28aa19a3e90a8de1743cc22cc79efac705e6
EdoardoDiLisio <edoardo.dl@libero.it>
2024-04-19 13:53:27 +0200

    fino al 4.11

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -39,4 +39,43 @@ for value in odds:
 	print(value)
 print('\n\n')
 
-#
+#4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
+
+three = list(range(3,31,3))
+for value in three:
+    print(value)
+print('\n\n')
+
+# 4.8 Cubes
+
+cubes = []
+for value in range(1,11):
+	cube = value**3
+	cubes.append(cube)
+	print(cube)
+print('\n\n')
+# 4.9 Cube Comprehension
+
+cubes_list = [value**3 for value in range(1,11)]
+print(cubes_list)
+print('\n\n')
+## 4.10 Slices
+
+print(cubes[:3])
+
+print(cubes[2:5])
+
+print(cubes[-3:])
+print('\n\n')
+# 4.11
+
+friend_pizzas = pizzas[:]
+pizzas.append('supreme')
+friend_pizzas.append('Diavola')
+print("My favorite pizzas are:")
+for pizza in pizzas:
+	print(pizza)
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+	print(pizza)
+print('\n\n')
