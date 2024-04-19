d16112f6c7783effc683feeaaa832bdfb83fbfe8
EdoardoDiLisio <edoardo.dl@libero.it>
2024-04-19 12:44:08 +0200

    fine esercizi lezione2 creazione lezione3 e inizio esercizi

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -184,3 +184,162 @@ print("No of guest coming in dinner :",len(invites))
 # For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. 
 # Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once.
 
+Lista = ['Mare', 'Montagna', "Lago", "Sci", 'Moto', 'Macchina']
+Lista.remove('Mare')
+Lista.append('Volo')
+Lista.pop(1)
+print(Lista)
+print(sorted(Lista))
+Lista.reverse()
+print(Lista)
+Lista.sort()
+print(Lista)
+
+# 6-1. Person: Use a dictionary to store information about a person you know. 
+# Store their first name, last name, age, and the city in which they live. 
+# You should have keys such as first_name, last_name, age, and city. 
+# Print each piece of information stored in your dictionary.
+
+person0 = {'first_name': 'Eric', 'last_name': 'Matthe', 'age': '43', 'city': 'Sitka'}
+
+print(person0['first_name'])
+print(person0['last_name'])
+print(person0['age'])
+print(person0['city'])
+
+# 6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers. 
+# Think of five names, and use them as keys in your dictionary. 
+# Think of a favorite number for each person, and store each as a value in your dictionary. 
+# Print each person’s name and their favorite number. 
+# For even more fun, poll a few friends and get some actual data for your program.
+
+favorite_numbers = {'mandy': 42, 'micah': 23, 'gus': 7, 'hank': 10, 'maggie': 9}
+
+num = favorite_numbers['mandy']
+print(f"Mandy's favorite number is {num}.")
+
+num = favorite_numbers['micah']
+print(f"Micah's favorite number is {num}.")
+
+num = favorite_numbers['gus']
+print(f"Gus's favorite number is {num}.")
+
+num = favorite_numbers['hank']
+print(f"Hank's favorite number is {num}.")
+
+num = favorite_numbers['maggie']
+print(f"Maggie's favorite number is {num}.")
+
+# 6-3. Glossary: A Python dictionary can be used to model an actual dictionary. 
+# However, to avoid confusion, let’s call it a glossary.
+# Think of five programming words you’ve learned about in the previous chapters. 
+# Use these words as the keys in your glossary, and store their meanings as values.
+# Print each word and its meaning as neatly formatted output. 
+# You might print the word followed by a colon and then its meaning, or print the word on one line and 
+# then print its meaning indented on a second line. 
+# Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+
+glossary : dict = {'print' : 'stampa', 'remove' : 'rimuove', 'append' : 'aggiunge', 'if' : 'loop condizione singola'}
+
+for word, significato in glossary.items():
+    print('Il significato di {} è: {}'.format(word, significato))
+
+# 6-7. People: Start with the program you wrote for Exercise 6-1. 
+# Make two new dictionaries representing different people, and store all three dictionaries in a list called people. 
+# Loop through your list of people. As you loop through the list, print everything you know about each person.
+
+person0 : dict = {'first_name': 'Eric', 'last_name': 'Matthe', 'age': '43', 'city': 'Sitka'}
+person1 : dict = {'first_name': 'Leonardo', 'last_name': 'Leo', 'age': '34', 'city': 'Paris'}
+person2 : dict = {'first_name': 'Valerio', 'last_name': 'Vale', 'age': '67', 'city': 'London'}
+persons  = list [person0, person1, person2]
+for person in persons:
+    print(person)
+
+# 6-8. Pets: Make several dictionaries, where each dictionary represents a different pet. 
+# In each dictionary, include the kind of animal and the owner’s name. Store these dictionaries in a list called pets. 
+# Next, loop through your list and as you do, print everything you know about each pet. 
+
+cookie = {
+	'species': 'cat',
+	'color': 'calico',
+	'owner': 'allie',
+}
+elsa = {
+	'species': 'cat',
+	'color': 'black',
+	'owner': 'william',
+}
+ernie = {
+	'species': 'dog',
+	'color': 'tan',
+	'owner': 'joe',
+}
+
+pets = [cookie, elsa, ernie]
+
+for pet in pets:
+	print(pet)
+
+# 6-9. Favorite Places: Make a dictionary called favorite_places. 
+# Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. 
+# To make this exercise a bit more interesting, ask some friends to name a few of their favorite places. 
+# Loop through the dictionary, and print each person’s name and their favorite places.
+
+favorite_places = {
+	'william': ['lake tahoe', 'maldives', 'talmo'],
+	'allie': ['talmo', 'mexico', 'bahamas'],
+	'luke': ['home', 'nanas', 'grams'],
+}
+
+for name, places in favorite_places.items():
+	print(name.title() + ", your favorite places are:")
+	for place in places:
+		print("\t" + place.title())
+
+# 6.10 Favorite Numbers
+# Modify your program from Exercise 6-2 (page 102) so
+# each person can have more than one favorite number. Then print each person’s
+# name along with their favorite numbers.
+fav_numbers = {
+	'luke': [2,4],
+	'william': [13,87],
+	'allie': [11,91],
+	'sookie': [0,100],
+	'elsa': [1,11],
+}
+
+for name, numbers in fav_numbers.items():
+	print(name.title() + ", your favorite numbers are: ")
+	for num in numbers:
+		print("\t" + str(num))
+
+# 6.11 Cities
+# Make a dictionary called cities. Use the names of three cities as
+# keys in your dictionary. Create a dictionary of information about each city and
+# include the country that the city is in, its approximate population, and one fact
+# about that city. The keys for each city’s dictionary should be something like
+# country, population, and fact. Print the name of each city and all of the information
+# you have stored about it.
+
+cities = {
+	'atlanta': {
+		'state': 'georgia',
+		'population': 5500000,
+		'fact': 'known as hotlanta',
+		},
+	'new york': {
+		'state': 'new york',
+		'population': 8500000,
+		'fact': 'big apple',
+		},
+	'los angeles': {
+	    'state': 'california',
+	    'population': 4000000,
+	    'fact': 'city of angels',
+	    },
+}
+
+for city, info in cities.items():
+	print(city + ":")
+	for k, v in info.items():
+		print("\t" + k + ":" + str(v))
