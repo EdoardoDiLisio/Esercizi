f777552791b75e97cc834a5d3ddebd1f439ce85b
EdoardoDiLisio <edoardo.dl@libero.it>
2024-05-08 12:18:54 +0200

    ese di spiegazione

--- Lezione6/classi.py
+++ Lezione6/classi.py
@@ -1,4 +1,30 @@
-class Persona:
+class Person:
+    def __init__(self, name:str, surname:str, ssn:str) -> None:
+        
+        self._name: str = name
+        self._surname: str = surname
+        self._ssn: str = ssn
     
-    def __init__(self) -> None
-        pass
+    def get_name(self):
+        
+        return  self._name
+    
+    def set_name(self, name: str) -> None:
+        """
+        this function set the attribute name
+        input: name : str, the parameter contains the user's name
+        input: name : str
+        return: none
+        """
+        
+        raise Exception('you cannot modify the name!')
+
+
+
+
+Person_1: Person = Person(name='Edoardo', surname='Di_Lisio', ssn='DLSDRD00E30H501A')
+
+print(Person_1.get_name())
+Person_1.set_name(name='giusy')
+
+print(Person_1.get_name())
